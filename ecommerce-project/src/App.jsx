32c3ebd8a3f9b4534d4521a915c73f6b80ffe428
Henry Vu <namkhanhvuduy0867@gmail.com>
2025-12-24 09:14:30 -0600

    Adding routing

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router';
 import { HomePage } from './Pages/HomePage'
+import { CheckoutPage } from './Pages/CheckoutPage'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route index element={<HomePage />}></Route> // index = path = ''
+      <Route path='checkout' element={<CheckoutPage />} />
+    </Routes>
   )
 }
 
