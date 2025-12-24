928a769940ebfb95b9ab9437619d19fa287e561e
Henry Vu <namkhanhvuduy0867@gmail.com>
2025-12-24 09:37:52 -0600

    Update routing + UI separation

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import { Routes, Route } from 'react-router';
 import { HomePage } from './Pages/HomePage'
 import { CheckoutPage } from './Pages/CheckoutPage'
+import { TrackingPage } from './Pages/TrackingPage'
+import { OrdersPage } from './Pages/OrdersPage'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -11,6 +13,8 @@ function App() {
     <Routes>
       <Route index element={<HomePage />}></Route> // index = path = ''
       <Route path='checkout' element={<CheckoutPage />} />
+      <Route path='tracking' element={<TrackingPage />} />
+      <Route path='orders' element={<OrdersPage />} />
     </Routes>
   )
 }
