10a27e8c9e956322e89c3307b6f6b4a042d0af46
Henry Vu <namkhanhvuduy0867@gmail.com>
2026-01-05 10:11:33 -0600

    Use beforeEach to reset test functions

--- ecommerce-project/src/Pages/home/Product.test.jsx
+++ ecommerce-project/src/Pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from 'vitest';
+import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { Product } from './Product';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
@@ -17,9 +17,23 @@ vi.mock('axios'); // Mock axios to prevent actual API calls
 // "userEvent" to simulate user interactions
 
 describe('Label: Product Component Integration Tests', () => {
+    let product = {
+        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+        rating: {
+            stars: 4.5,
+            count: 87
+        },
+        priceCents: 1090,
+        keywords: ["socks", "sports", "apparel"]
+    }
 
-    it('displays the product details correctly', async () => {
-        const product = {
+    let loadCart = vi.fn(); // Fake function that does nothing
+
+    // test hook
+    beforeEach(() => {
+        product = {
             id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             image: "images/products/athletic-cotton-socks-6-pairs.jpg",
             name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -31,9 +45,11 @@ describe('Label: Product Component Integration Tests', () => {
             keywords: ["socks", "sports", "apparel"]
         }
 
-        const loadCart = vi.fn(); // Fake function that does nothing
+        loadCart = vi.fn(); // reset fake function before each test
+    })
 
-        render(<Product product={product} loadCart={loadCart} />);
+    it('displays the product details correctly', async () => {
+        render(<Product product={product} loadCartItems={loadCart} />);
 
         const user = userEvent.setup();
         const addToCartButton = screen.getByTestId('add-to-cart-button');
